1aa9cf63c39b82727ba046a125a3b6b246e8b19b
asaintpi <131413334+asaintpi@users.noreply.github.com>
2024-11-13 13:51:46 -0800

    tests done

--- craw.test.js
+++ craw.test.js
@@ -1,4 +1,5 @@
 const { normalizeUrl } = require('./crawl.js')
+const { getURLsFromHTML } = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL test', () => {
@@ -15,13 +16,56 @@ test('trailing slash', () => {
     expect(actual).toEqual(expected)
 })
 
-test('capitals test', () => {
-    const input = 'https://BLOG.boot.dev/path/'
-    const actual = normalizeUrl(input)
-    const expected = 'blog.boot.dev/path/'
+test('getURLsFromHTML', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev">
+                Boot.dev Blog 
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(input, inputBaseURL)
+    const expected = ['https://blog.boot.dev/']
     expect(actual).toEqual(expected)
 })
 
-test('strip http', () => {
-    
+test('getURLsFromHTML relative', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="/path/">
+                Boot.dev Blog 
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(input, inputBaseURL)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative and absolute', () => {
+    const input = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog 1
+            </a>
+            <a href="/path2/">
+                Boot.dev Blog 2 
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://blog.boot.dev'
+    const actual = getURLsFromHTML(input, inputBaseURL)
+    const expected = ['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/']
+    expect(actual).toEqual(expected)
 })
--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('jsdom')
+
+
+function getURLsFromHTML(htmlbody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlbody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) == '/'){
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(`${linkElement}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with abs url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeUrl(urlString) {
     const fnUrl = new URL(urlString)
     const output = `${fnUrl.hostname}${fnUrl.pathname}`
@@ -5,10 +34,11 @@ function normalizeUrl(urlString) {
     if (output.length > 0 && output.slice(-1) == '/') {
         return output.slice(0, -1)
     }
-    
+
     return output
 }
 
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getURLsFromHTML
 }
